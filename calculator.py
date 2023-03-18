2af015b6be8031ceed736569f162f386af67b51a
NovykovDaniil2 <relax2934@gmail.com>
2023-03-18 20:14:16 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+class Calculator:
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
+        master.configure(bg='gray')
+
+        # create display
+        self.display = tk.Entry(master, width=20, font=('Arial', 20))
+        self.display.grid(row=0, column=0, columnspan=4, pady=5)
+
+        # create buttons
+        self.create_button('7', 1, 0)
+        self.create_button('8', 1, 1)
+        self.create_button('9', 1, 2)
+        self.create_button('/', 1, 3)
+
+        self.create_button('4', 2, 0)
+        self.create_button('5', 2, 1)
+        self.create_button('6', 2, 2)
+        self.create_button('*', 2, 3)
+
+        self.create_button('1', 3, 0)
+        self.create_button('2', 3, 1)
+        self.create_button('3', 3, 2)
+        self.create_button('-', 3, 3)
+
+        self.create_button('0', 4, 0)
+        self.create_button('.', 4, 1)
+        self.create_button('C', 4, 2)
+        self.create_button('+', 4, 3)
+
+        self.create_button('=', 5, 0, columnspan=4)
+
+    def create_button(self, text, row, column, columnspan=1):
+        button = tk.Button(self.master, text=text, font=('Arial', 16), width=5, height=2, bg='#131313', fg='white', command=lambda: self.button_click(text))
+        button.grid(row=row, column=column, columnspan=columnspan, padx=5, pady=5)
+
+    def button_click(self, text):
+        if text == '=':
+            try:
+                result = eval(self.display.get())
+                self.display.delete(0, tk.END)
+                self.display.insert(0, str(result))
+            except:
+                self.display.delete(0, tk.END)
+                self.display.insert(0, 'Error')
+        elif text == 'C':
+            self.display.delete(0, tk.END)
+        else:
+            self.display.insert(tk.END, text)
+
+
+def start_calc():
+    root = tk.Tk()
+    calculator = Calculator(root)
+    root.mainloop()
+    return 'Thank you for using our calculator'
+
+print(start_calc())
